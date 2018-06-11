28899d3391ed6845d58383c2b696fb6f40346162
November Williams <nnwilliams8@gmail.com>
2018-06-11 14:36:45 -0400

    um

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,21 @@
-function swap(a, b) {
-  return a > b ? 1 : 0;
-}
-
 function bubbleSort(arr, comparator) {
-  let i = 0;
-  while (arr[i + 1]) {
-    let currentIndex = arr[i];
-    if (comparator(arr[i], arr[i + 1])) {
-      arr[i] = arr[i + 1];
-      arr[i + 1] = currentIndex;
+  if (!comparator) {
+    comparator = function(a, b) {
+      return a > b ? 1 : 0;
+    };
+  }
+
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 1; i < arr.length; i++) {
+      let currentIndex = arr[i - 1];
+      if (comparator(arr[i - 1], arr[i])) {
+        arr[i - 1] = arr[i];
+        arr[i] = currentIndex;
+        swapped = true;
+      }
     }
-    i++;
   }
   return arr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,12 +1,9 @@
 describe('Bubble Sort', function() {
-  function swap(a, b) {
-    return a > b ? 1 : 0;
-  }
-
   it('handles an empty array', function() {
-    expect(bubbleSort([], swap)).toEqual([]);
+    expect(bubbleSort([])).toEqual([]);
   });
+
   it('sorts an array of numbers', function() {
-    expect(bubbleSort([3, 4, 1, 9, 8, 5]), swap).toEqual([1, 3, 4, 5, 8, 9]);
+    expect(bubbleSort([3, 4, 1, 9, 8, 5])).toEqual([1, 3, 4, 5, 8, 9]);
   });
 });
